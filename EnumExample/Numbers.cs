e0db2347fda1fd116386bea0ddfba955c26ec3f8
Pavel Berezkin <Berezkin.P@dns-shop.ru>
2020-07-14 15:53:34 +1000

    Добавил примеры с явным указанием значений в перечислениях

---
+++ EnumExample/Numbers.cs
@@ -0,0 +1,12 @@
+﻿namespace EnumExample
+{
+    // Явное указание значений для всех элементов
+    
+    public enum Numbers
+    {
+        Two = 2,
+        Four = 4,
+        Eight = 8,
+        Sixteen = 16
+    }
+}
---
+++ EnumExample/Operation.cs
@@ -0,0 +1,12 @@
+﻿namespace EnumExample
+{
+    // Явное указание значений элементов
+    
+    public enum Operation
+    {
+        Add = 1,  // каждый следующий элемент увеличивается на 1
+        Subtract, // 2
+        Multiply, // 3
+        Divide    // 4
+    }
+}
