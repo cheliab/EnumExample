dbe243d0e3b54fc249060d27a775cd7b2866f094
Pavel Berezkin <Berezkin.P@dns-shop.ru>
2020-07-14 15:40:36 +1000

    Добавил перечисления Days и Time

---
+++ EnumExample/Days.cs
@@ -0,0 +1,16 @@
+﻿namespace EnumExample
+{
+    /// <summary>
+    /// Дни недели
+    /// </summary>
+    public enum Days
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
---
+++ EnumExample/Time.cs
@@ -0,0 +1,13 @@
+﻿namespace EnumExample
+{
+    /// <summary>
+    /// Время дня
+    /// </summary>
+    public enum Time : byte
+    {
+        Morning,
+        Afternoon,
+        Evening,
+        Night
+    }
+}
