bb2204714f5875f858707f2e015db26e649bde61
Pavel Berezkin <Berezkin.P@dns-shop.ru>
2020-07-14 16:00:44 +1000

    Добавил пример с одинаковыми значениями в перечислении

---
+++ EnumExample/Color.cs
@@ -0,0 +1,13 @@
+﻿namespace EnumExample
+{
+    /// <summary>
+    /// Пример с одинаковыми значениями в перечислении
+    /// </summary>
+    public enum Color
+    {
+        White = 1,
+        Black = 2,
+        Green = 2,
+        Blue = White // Blue = 1
+    }
+}
