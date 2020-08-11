00caf521849f726f26efb2dcbca314e5e4981045
Berezkin Pavel <Berezkin.P@dns-shop.ru>
2020-08-12 00:48:59 +1000

    Добавлен класс IdentityValueAttribute

---
+++ EnumExample/Attributes/IdentityValueAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EnumExample.Attributes
+{
+    /// <summary>
+    /// Значение идентификатора в базе данных
+    /// </summary>
+    public sealed class IdentityValueAttribute : Attribute
+    {
+        private string _value;
+
+        public IdentityValueAttribute(string value)
+        {
+            this._value = value;
+        }
+        
+        public string Value => _value;
+    }
+}
